36bb7030663647b18acc205192e251156f71a00e
Lubomur Chuchvara <lubomircucvara@gmail.com>
2024-08-03 16:37:23 +0300

    phonetesting_app

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import { PhonePage } from './pages/PhonesPage';
 import { FavoritesPage } from './pages/FavoritesPage';
@@ -8,6 +8,7 @@ import { GlobalProvider } from './Context/GlobalContext';
 import { PageNotFound } from './pages/PageNotFound';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage'; // Додайте цей імпорт
 import { Provider } from 'react-redux';
 import './firebase';
 import { store } from './store';
@@ -19,16 +20,18 @@ export const Root = () => {
         <GlobalProvider>
           <Routes>
             <Route path="/" element={<App />}>
-              <Route path="register" element={<RegisterPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="phones">
-                <Route index element={<PhonePage />} />
-                <Route path=":productId" element={<ProductDetailsPage />} />
-              </Route>
+              <Route path="phones" element={<PhonePage />} />
               <Route path="favourites" element={<FavoritesPage />} />
               <Route path="cart" element={<CartPage />} />
+              <Route
+                path="product/:productId"
+                element={<ProductDetailsPage />}
+              />
               <Route path="*" element={<PageNotFound />} />
             </Route>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />{' '}
           </Routes>
         </GlobalProvider>
       </Router>
